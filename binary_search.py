1ed2f623c21f5d7e57979418ce6136a3c25f9f3c
YashrajSKasana <yashrjskasana2007@gmail.com>
2024-11-13 23:47:30 +0530

    corecte the code a litel

--- binary_search.py
+++ binary_search.py
@@ -1,9 +1,9 @@
 from math import floor
-
+first: bool = True
 def binary_search(num_list: list[int] | list[tuple[int, int]], num_to_find: int) -> int | str:
-    if type(num_list[0]) is int:
-        if num_list[0] == num_to_find: return 0
-        num_list = list(enumerate(num_list))
+    if first:
+        if num_list[0][1] == num_to_find: return 0
+        first = False
     if len(num_list) == 1: return "not found"
     mid_index = floor(len(num_list)/2)
     mid_item = num_list[mid_index]
@@ -13,6 +13,6 @@ def binary_search(num_list: list[int] | list[tuple[int, int]], num_to_find: int)
 
 @lambda _: __name__ == '__main__' and _()
 def main() -> None:
-    num_list: list[int] = list(map(int, filter(None, input().split(" "))))
+    num_list: list[int] = list(enumerate(map(int, filter(None, input().split(" ")))))
     num_to_find: int = int(input())
     print(binary_search(num_list, num_to_find))
