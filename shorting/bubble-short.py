b99f8c5f08695b10be930b23f99ebfa1ec541266
YashrajSKasana <yashrjskasana2007@gmail.com>
2024-11-14 14:44:43 +0530

    changed miner things in bubble short algo.

--- shorting/bubble-short.py
+++ shorting/bubble-short.py
@@ -1,11 +1,15 @@
 def bubble_short(num_list):
     unsorted_list_len = len(num_list)
     while unsorted_list_len > 0:
+        swapped = False
         for i in range(1,unsorted_list_len):
             pre = num_list[i-1]
             cur = num_list[i]
             if pre > cur:
                 num_list[i], num_list[i-1] = pre, cur
+                swapped =True
+        if not swapped:
+            break
         unsorted_list_len -= 1
     return num_list
 
